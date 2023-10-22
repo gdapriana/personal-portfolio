94133d33a31b1a7d20e99669305044af1c2cba6e
icequeenwand <gedeapriana36@gmail.com>
2023-10-23 00:26:02 +0800

    project design config

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { Square2StackIcon, ListBulletIcon } from "@heroicons/react/24/outline";
+import LaravelIcon from "@/assets/images/laravel.png"
 
 const metadata: any = {
 	title: "Project",
@@ -14,6 +15,7 @@ export default function Project() {
 			<Header />
 			<Divider />
 			<ViewButton />
+			<ProjectSection />
 		</div>
 	);
 }
@@ -44,6 +46,32 @@ const ViewButton = () => {
 		</div>
 	);
 };
+
+const ProjectSection = () => {
+	return (
+		<div className="w-full gap-2 grid mt-4 grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 md:grid-cols-2">
+			<ProjectCard />
+			<ProjectCard />
+			<ProjectCard />
+			<ProjectCard />
+		</div>
+	)
+}
+
+const ProjectCard = () => {
+	return (
+		<div className="max-w-sm flex flex-col relative overflow-hidden bg-white border border-gray-200 rounded-lg dark:bg-neutral-800 dark:border-neutral-700">
+			<div className="w-full bg-cover h-12" style={{backgroundImage: "url(https://source.unsplash.com/random/1920x1080?programming)"}}></div>
+			<div className="w-12 rounded-full m-6 h-12 absolute" style={{backgroundImage: `src(${LaravelIcon})`}}>
+			</div>
+			<div className="w-full h-32 px-2 pb-2 pt-6">
+				<div className="">
+					<h1 className="font-bold text-neutral-600 text-sm">Indonesian Literature</h1>
+				</div>
+			</div>
+		</div>
+	)
+}
 
 const Divider = () => {
 	return <div className="w-full border-b my-4"></div>;
