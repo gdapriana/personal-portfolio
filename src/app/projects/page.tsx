5e5c9ce47ed0e329432f0b4f5410d8027a616924
icequeenwand <gedeapriana36@gmail.com>
2023-10-23 18:01:38 +0800

    finishin project section (card and list) & insert data to mongodb

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -17,16 +17,15 @@ const metadata: any = {
 export default function Project() {
   const [view, setView] = useState("card");
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full overflow-hidden">
       <Header />
       <Divider />
-      <ViewButton />
-      <ProjectSection />
+      <ViewButton view={view} setView={setView} />
+      <ProjectSection view={view} />
       <Pagination />
     </div>
   );
 }
-
 const Header = () => {
   return (
     <header className="flex flex-col justify-center items-start gap-2">
@@ -37,37 +36,52 @@ const Header = () => {
     </header>
   );
 };
-
-const ViewButton = () => {
+const ViewButton = ({ view, setView }: { view: string; setView: any }) => {
   return (
     <div className="w-full flex justify-end items-center gap-2">
       <p className="text-sm text-neutral-400">Change View</p>
       <div
-        data-tooltip-target="cardview-tooltip"
-        className="border p-1 rounded-md text-neutral-600 dark:border-neutral-800 cursor-pointer bg-neutral-100 dark:bg-neutral-800"
+        onClick={() => setView("card")}
+        className={`${
+          view === "card" ? "bg-neutral-100" : ""
+        } border p-1 rounded-md text-neutral-600 dark:border-neutral-800 cursor-pointer dark:bg-neutral-800`}
       >
         <Square2StackIcon className="w-5" />
       </div>
 
-      <div className="border dark:border-neutral-800 p-1 rounded-md text-neutral-600 cursor-pointer">
+      <div
+        onClick={() => setView("list")}
+        className={`${
+          view === "list" ? "bg-neutral-100" : ""
+        } border dark:border-neutral-800 p-1 rounded-md text-neutral-600 cursor-pointer`}
+      >
         <ListBulletIcon className="w-5" />
       </div>
     </div>
   );
 };
-const ProjectSection = () => {
-  return (
-    <div className="w-full gap-2 grid mt-4 grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 md:grid-cols-2">
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-    </div>
-  );
+const ProjectSection = ({ view }: { view: string }) => {
+  if (view === "card") {
+    return (
+      <div className="w-full gap-2 grid mt-4 grid-cols-2 sm:grid-cols-1 lg:grid-cols-3 md:grid-cols-2">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </div>
+    );
+  } else {
+    return (
+      <div className="w-full mt-4 flex flex-col justify-center gap-2 items-center overflow-hidden">
+        <ProjectList />
+        <ProjectList />
+      </div>
+    );
+  }
 };
 const ProjectCard = () => {
   return (
-    <div className="max-w-sm flex flex-col relative overflow-hidden bg-white border border-gray-200 rounded-lg dark:bg-neutral-900 dark:border-neutral-800">
+    <div className="max-w-sm flex flex-col relative overflow-hidden bg-white border border-gray-200 rounded-xl dark:bg-neutral-900 dark:border-neutral-800">
       <div
         className="w-full bg-cover grayscale h-12"
         style={{
@@ -112,6 +126,34 @@ const ProjectCard = () => {
             </div>
           </div>
         </div>
+      </div>
+    </div>
+  );
+};
+const ProjectList = () => {
+  return (
+    <div className="w-full gap-2 border-b p-4 flex justify-center items-center">
+      <div className="">
+        <Image className="w-8" src={LaravelIcon} alt={"icon"} />
+      </div>
+      <div className="flex-1">
+        <h1 className="text-base font-bold text-neutral-600">Project Name</h1>
+      </div>
+      <div className="gap-2 hidden md:flex overflow-x-auto justify-center items-center">
+        <Image src={PythonIcon} alt="python" className="grayscale w-4" />
+        <Image src={NumPyIcon} alt="python" className="grayscale w-4" />
+        <Image src={PandasIcon} alt="python" className="grayscale h-4 w-4" />
+      </div>
+      <div className="">
+        <Link
+          href="http://localhost:3000"
+          className="flex gap-1 dark:border-neutral-800 dark:bg-neutral-800 text-neutral-400 border py-1 px-2 rounded-md text-xs justify-center items-center"
+        >
+          View{" "}
+          <div className="bg-white rounded-full">
+            <Image className="w-4" src={GithubIcon} alt="github" />
+          </div>
+        </Link>
       </div>
     </div>
   );
--- src/models/project.ts
+++ src/models/project.ts
@@ -1,12 +1,18 @@
 import mongoose, { Schema } from "mongoose";
 
-const projectSchema = new Schema({
-  title: String,
-  description: String,
-  technology: Array
-}, {
-  timestamps: true
-})
+const projectSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    language: String,
+    technology: Array,
+    github: String,
+  },
+  {
+    timestamps: true,
+  },
+);
 
-const Project = mongoose.models.Project || mongoose.model("Project", projectSchema)
-export default Project
+const Project =
+  mongoose.models.Project || mongoose.model("Project", projectSchema);
+export default Project;
